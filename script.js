2c86b7b8220812d11b94288cb7ccbfa8cf01fe45
Bhakti Nannor <140268933+BhaktiNannor@users.noreply.github.com>
2025-12-09 23:20:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio loaded successfully!");
+const sections = document.querySelectorAll('.slide-in');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    const trigger = window.innerHeight - 100;
+
+    if (top < trigger) {
+      section.style.animationPlayState = 'running';
+    }
+  });
+});
